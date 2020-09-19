ff9db620e09f65ca906be78fa846160683250e7a
isha2825 <64250083+isha2825@users.noreply.github.com>
2020-09-19 19:01:33 +0000

    Add files via upload

---
+++ 00_Introduction-Main-Goals-of-the-Project.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Tasks
+
+# - Optical Flow
+# - Dense Optical Flow
+# - MeanShift Tracking
+# - CamShift Tracking
+# - Single Tracking
+# - Multi Tracking
+
+# You can Run the cell by:
+# - clicking on the Run button
+# - Shift + Enter (<b>Windows</b>).
+# - Control + Enter (<b>Mac</b>)
+
+# When we have finished with our Jupyter Notebook we go to:
+
+# <b>File</b> -> <b>Save and Checkpoint</b> and then
+
+# <b>File</b> -> <b>Close and Halt</b>
+
+# ### How to Tune Cascade Classifier
+
+# (https://docs.opencv.org/2.4/modules/objdetect/doc/cascade_classification.html#cascadeclassifier-detectmultiscale)
+# 
+# <b>detectMultiscale</b>(input image, <b>Scale Factor</b>, <b>Min Neighbors</b>)
+# - <b>Scale Factor</b> Parameter specifying how much yhe image size is reduced at each image scale.
+# 
+# 
+# - <b>Min Neighbors</b> Parameter specifying how many neighbors each candidate rectangle should have to retain it.
+
+# ### HSV colorspace
+# 
+# https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
+
+# In[ ]:
+
+
+
+
